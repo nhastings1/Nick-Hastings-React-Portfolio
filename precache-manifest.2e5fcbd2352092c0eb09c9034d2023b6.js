f8b2ccaac58d557231d5d1ee6c299d5afa085853
Nick Hastings <nickhastings@Clients-MacBook-Pro-2.local>
2022-11-16 14:46:19 -0600

    Updates

---
+++ precache-manifest.2e5fcbd2352092c0eb09c9034d2023b6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48f1b902310e7a2b9346f440c0515fe",
+    "url": "/Nick-Hastings-React-Portfolio/index.html"
+  },
+  {
+    "revision": "c6d5dc5a81b87a96e0d8",
+    "url": "/Nick-Hastings-React-Portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "a29fc90562395b1b0375",
+    "url": "/Nick-Hastings-React-Portfolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "6ecb9b774551595b7b6a",
+    "url": "/Nick-Hastings-React-Portfolio/static/css/main.f92ad186.chunk.css"
+  },
+  {
+    "revision": "8f8b2d14ef4be0c64ec7",
+    "url": "/Nick-Hastings-React-Portfolio/static/js/2.e6dbcfcc.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/Nick-Hastings-React-Portfolio/static/js/2.e6dbcfcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d5dc5a81b87a96e0d8",
+    "url": "/Nick-Hastings-React-Portfolio/static/js/3.0cff11bf.chunk.js"
+  },
+  {
+    "revision": "a29fc90562395b1b0375",
+    "url": "/Nick-Hastings-React-Portfolio/static/js/4.2e4f91f7.chunk.js"
+  },
+  {
+    "revision": "6ecb9b774551595b7b6a",
+    "url": "/Nick-Hastings-React-Portfolio/static/js/main.be150dab.chunk.js"
+  },
+  {
+    "revision": "df4f692a2d0ea4692e52",
+    "url": "/Nick-Hastings-React-Portfolio/static/js/runtime-main.3f900125.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/Agustina.d41d8cd9.woff"
+  },
+  {
+    "revision": "4c7c39201cdd1a93e4bc6ac01a14583c",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/Concert.4c7c3920.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/Montserrat-Regular.d41d8cd9.ttf"
+  },
+  {
+    "revision": "39461d668badf0d242ba2f18233e15af",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/Movie.39461d66.png"
+  },
+  {
+    "revision": "ca067e43432a28dc2994634ff351b181",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/PG.ca067e43.png"
+  },
+  {
+    "revision": "5b75625b8d21e31ccc310eb65dea2319",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/Quiz.5b75625b.png"
+  },
+  {
+    "revision": "63b10a338458015392515b74a13515b7",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/Sweet.63b10a33.png"
+  },
+  {
+    "revision": "04d61539fb848941fb0227dd8379fec1",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/WeatherAPI.04d61539.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "6083a62f0b86a445ff3ef7584c2d2d45",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/ku.6083a62f.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Nick-Hastings-React-Portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
